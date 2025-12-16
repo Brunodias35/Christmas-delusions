97420420b65448daffa3d0c3cc29d498de87f668
ALUNO ALUNO <ALUNO@LABCHARURI.LOCAL>
2025-12-16 19:48:36 -0300

    push inicial do projeto

---
+++ Game_jam/Assets/codigos/adolecente.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+
+
+public class adolecente : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+    [SerializeField] GameObject presente;
+    [SerializeField] GameObject carvao;
+    [SerializeField] GameObject CasaBoa;
+    [SerializeField] GameObject CasaRuim;
+    [SerializeField] GameObject CasaNeutra;
+    float Velocidade = 10f;
+    float spedUp = 7f;
+    float spedBaixo = 7f;
+    float spedDireita = 7f;
+    float timeCasa = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+
+        int next = Random.Range(0, 4);
+
+        if (transform.position.y <= 4.30f)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += new Vector3(0f, spedUp * Time.deltaTime, 0f);
+            }
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(spedDireita * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(spedDireita * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0f, spedUp * Time.deltaTime, 0f);
+        }
+
+        // instancia o presente e o carvao
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Instantiate(presente, transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(carvao, transform.position, Quaternion.identity);
+        }
+
+        //intacia as casa random
+        timeCasa += Time.deltaTime;
+        if(timeCasa == 3f)
+        {
+
+            if (next == 1)
+            {
+                Instantiate(CasaBoa, transform.position = new Vector3(12.64f, -2.77f, 0f), Quaternion.identity);
+            }
+            if (next == 2)
+            {
+                Instantiate(CasaRuim, transform.position = new Vector3(12.64f, -2.77f, 0f), Quaternion.identity);
+            }
+            if (next == 3)
+            {
+                Instantiate(CasaNeutra, transform.position = new Vector3(12.64f, -2.77f, 0f), Quaternion.identity);
+            }
+
+        }
+           
+    }
+       
+    
+  
+}
